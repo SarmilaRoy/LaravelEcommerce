0842600705815733dd49bbc1a8d2786cc1497895
SarmilaRoy <sarmilaroy583032@gmail.com>
2022-07-07 01:07:54 +0600

    Update Quantity

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -10,6 +10,8 @@ use Cart;
 
 class ShopComponent extends Component
 {
+
+    
     public function store($product_id,$product_name,$product_price){
         Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         session()->flash('cart-msg','Item Add in Cart');
