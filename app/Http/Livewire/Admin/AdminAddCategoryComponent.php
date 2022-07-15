f72879c296c057a716f9787c378b6bfbd69a4bc1
SarmilaRoy <sarmilaroy583032@gmail.com>
2022-07-16 00:47:16 +0600

    Admin Category Added

---
+++ app/Http/Livewire/Admin/AdminAddCategoryComponent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+use Illuminate\Support\Str;
+use App\Models\Category;
+
+class AdminAddCategoryComponent extends Component
+{
+    public $name;
+    public $slug;
+    public function generateSlug(){
+        $this->slug=Str::slug($this->name);
+    }
+    public function storeCategory(){
+        $category=new Category();
+        $category->name=$this->name;
+        $category->slug=$this->slug;
+        $category->save();
+        session()->flash("msg","Category has been created Successfully!!");
+    }
+    public function render()
+    {
+        return view('livewire.admin.admin-add-category-component')->layout('layouts.base');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Livewire\ProductDetailsComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -55,4 +56,5 @@ Route::middleware(['auth:sanctum','verified'])->group(function () {
 Route::middleware(['auth:sanctum','verified','authadmin'])->group(function () {
     Route::get('/admin/deshboard',AdminDeshboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addCategory');
 });
