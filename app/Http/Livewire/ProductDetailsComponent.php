169ffcdbf842f11e5806736db1df482defd55c6c
SarmilaRoy <sarmilaroy583032@gmail.com>
2022-07-18 14:06:45 +0600

    Product details of add to cart added solve

--- app/Http/Livewire/ProductDetailsComponent.php
+++ app/Http/Livewire/ProductDetailsComponent.php
@@ -14,7 +14,7 @@ class ProductDetailsComponent extends Component
     }
 
     public function store($product_id,$product_name,$product_price){
-        Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         session()->flash('cart-msg','Item Add in Cart');
         return redirect()->route('product.cart');
     }
