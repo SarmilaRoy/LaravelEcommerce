812b7d53ecc080e74b71e2a8577d93d39462754c
SarmilaRoy <sarmilaroy583032@gmail.com>
2022-07-18 14:44:39 +0600

    Move Product From Wishlist to Cart

--- app/Http/Livewire/WishlistComponent.php
+++ app/Http/Livewire/WishlistComponent.php
@@ -4,7 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Cart;
-
+use App\Models\Product;
 class WishlistComponent extends Component
 {
     public function romoveFormWishlist($product_id){
@@ -17,10 +17,19 @@ class WishlistComponent extends Component
         }
       }
 
-      public function store($product_id,$product_name,$product_price){
-        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
-        session()->flash('cart-msg','Item Add in Cart');
-        return redirect()->route('product.cart');
+    //   public function store($product_id,$product_name,$product_price){
+    //     Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+    //     session()->flash('cart-msg','Item Add in Cart');
+    //     return redirect()->route('product.cart');
+    // }
+
+    public function moveProductFromWishlistToCart($rowId){
+      $item=Cart::instance('wishlist')->get($rowId);
+      Cart::instance('wishlist')->remove($rowId);
+      Cart::instance('cart')->add($item->id,$item->name,1,$item->price)->associate('App\Models\Product');
+      $this->emitTo('wishlist-count-component','refreshComponent');
+      $this->emitTo('cart-count-component','refreshComponent');
+      return redirect()->route('product.wishlist');
     }
   
     public function render()
