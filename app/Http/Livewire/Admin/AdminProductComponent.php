cec8e962d62689cf46f5a352207e004bba1cf8c9
SarmilaRoy <sarmilaroy583032@gmail.com>
2022-08-12 20:05:30 +0600

    Add Search on Admin Products Page

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -9,28 +9,37 @@ use App\Models\Product;
 class AdminProductComponent extends Component
 {
     use WithPagination;
+    public $searchTerm;
 
-    public function deleteProduct($id){
-        $product=Product::find($id);
-        if($product->image){
-            unlink('assets/images/products'.'/'.$product->image);
+    public function deleteProduct($id)
+    {
+        $product = Product::find($id);
+        if ($product->image) {
+            unlink('assets/images/products' . '/' . $product->image);
         }
-        if($product->images){
-            $images=explode(',',$product->images);
-            foreach($images as $image){
-              if($image){
-                unlink('assets/images/products'.'/'.$image);
-              }
+        if ($product->images) {
+            $images = explode(',', $product->images);
+            foreach ($images as $image) {
+                if ($image) {
+                    unlink('assets/images/products' . '/' . $image);
+                }
             }
         }
         $product->delete();
-        session()->flash("msg","Product has been deleted Successfully!!");
-
+        session()->flash('msg', 'Product has been deleted Successfully!!');
     }
 
     public function render()
     {
-        $products=Product::paginate(8);
-        return view('livewire.admin.admin-product-component',['products' => $products])->layout('layouts.base');
+        $search = '%' . $this->searchTerm . '%';
+        $products = Product::where('name', 'LIKE', $search)
+            ->orWhere('stock_status', 'LIKE', $search)
+            ->orWhere('regular_price', 'LIKE', $search)
+            ->orWhere('sale_price', 'LIKE', $search)
+            ->orderBy('id','DESC')
+            ->paginate(8);
+        return view('livewire.admin.admin-product-component', [
+            'products' => $products,
+        ])->layout('layouts.base');
     }
 }
